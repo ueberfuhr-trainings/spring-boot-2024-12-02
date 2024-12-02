c03bc3232116b21fa57107cfbc50c99de8e07d33
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-12-02 15:53:33 +0100

    Add test for GET /customers

---
+++ src/test/java/de/schulung/spring/accounts/CustomerApiTests.java
@@ -0,0 +1,32 @@
+package de.schulung.spring.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CustomerApiTests {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  void shouldReturnCustomers() throws Exception {
+    mockMvc
+      .perform(
+        get("/customers")
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isOk())
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+  }
+
+}
